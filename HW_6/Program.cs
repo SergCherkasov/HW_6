8b96dd6dc43e99cca1878b0ca7eea62275c9196a
Admin <Admin@DESKTOP-34GRT87>
2023-02-22 13:03:02 +0200

    Add project files.

---
+++ HW_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using HW_6.ValuesOperations;
+
+int a = 5;
+int b = 6;
+
+var operations = new Operations();
+
+Console.WriteLine($"Values before SWAP: a={a}, b={b}");
+
+operations.Swap(ref a, ref b);
+
+Console.WriteLine($"Values after SWAP: a={a}, b={b}");
+Console.WriteLine();
+
+int c = 123456;
+int amount;
+
+operations.CountFigures(c, out amount);
+Console.WriteLine($"Amount of figures in number: {amount}");
+Console.WriteLine();
+
+string str = "Something";
+int d = 5;
+string symbol;
+
+operations.ReturnSymbol(str,d,out symbol);
+Console.WriteLine($"Word for work: {str}");
+Console.WriteLine($"Number of searching symbol:{d}");
+Console.WriteLine($"Symbol to Return: {symbol}");
+
---
+++ HW_6/ValuesOperations/Operations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_6.ValuesOperations
+{
+    internal class Operations
+    {
+        public void Swap(ref int a, ref int b)
+        {
+            int temp=a; 
+            a=b;
+            b=temp;
+        }
+        public void CountFigures(int a,out int amount)
+        {
+            amount = 0;
+            
+            for(int i=0;i<a.ToString().Length;i++)
+            {
+                amount++;
+            }
+            
+        }
+        public void ReturnSymbol(string str,int d,out string symbol)
+        {
+            symbol=string.Empty;
+            for(int i=0;i<str.Length;i++)
+            {
+                if(i==d)
+                    symbol = str[i].ToString();
+            }
+        }
+    }
+}
